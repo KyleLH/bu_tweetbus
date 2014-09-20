e3c0a61a40371d9433956dcebef315b00aa8a29a
Kyle Holzinger <kylelholzinger@gmail.com>
2014-09-19 22:14:08 -0400

    Intermediate commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+var TwitterBot = require("node-twitterbot").TwitterBot;
+
+var bot = new TwitterBot({
+    'consumer_key': 'k26SEC2WkLuDTS7sI0Off97wt',
+    'consumer_secret': 'GcgwnuJiFlgF19u9ZwA2NenmqEttFkccKp7D4KcWoojEOpMGqh',
+    'access_token': '2775863764-aQ1jjei1smviiGrU6UHixb9y9h4LZuWlIgWsYLU',
+    'access_token_secret': 'MC7EltAHW7u1cZirzFvvYywdUI7noDhBdoW5oUUC3Vau9'
+});
+
+bot.addAction('tweet', function (twitter, action, tweet) {
+    console.log(bot.tweet('a tweet from node'));
+});
+
+bot.now('tweet');
