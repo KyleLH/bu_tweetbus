f1cee80aa3c08ca4a4b11acf885c171987b384e2
Kyle Holzinger <kylelholzinger@gmail.com>
2014-09-22 18:56:15 -0400

    Finished product

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var twitter = new twitterAPI({
     consumerSecret: keys.consumerSecret
 });
 var stops = {};
-var buses = [];
+var buses = {};
 
 // retrieve bus stops
 var url = 'http://www.bu.edu/bumobile/rpc/bus/stops.json.php?service_id=fall';
@@ -38,25 +38,37 @@ setInterval (function () {
             url: url,
             json: true,
         },
+
         function (e, res, body) {
-            if (!e && res.statusCode === 200) {
+            if (!e && res.statusCode === 200 && body.ResultSet) {
                 var allBuses = body.ResultSet.Result;
                 for (i in allBuses) {
-                console.log(allBuses[i]);
                     if (allBuses[i].arrival_estimates) {
-                        buses.push({})
-                        var index = len(buses)-1
-                        buses[index].prev_stop = buses[index].arrival_estimates[0].stop_id;
-                        buses[index].estimates = allBuses[i];
-                        console.log("Buses: ");
-                        console.log(buses);
-                        /*if (buses[index].prev_stop != buses[index].estimates.arrival_estimates[0].stop_id) {
-                            // tweet
-                            var status = "A bus just passed " + buses[index].prev_stop + ". \nNext stop: " +
-                                buses[index].estimates.arrival_estimates[0].stop_id + "\nETA: " +
+                        var index = buses.length-1
+                        cur_bus = allBuses[i].call_name
+
+                        // initialize buses
+                        if (!(buses[cur_bus])) {
+                            console.log("Initiate bus at " + cur_bus);
+                            buses[cur_bus] = {};
+                            buses[cur_bus].estimate = allBuses[i].arrival_estimates[0];
+                        }
+                        
+                        // check if just passed a stop
+                        if(buses[cur_bus].estimate.stop_id != allBuses[i].arrival_estimates[0].stop_id) {
+                            var date = new Date(allBuses[i].arrival_estimates[0].arrival_at);
+                            var tod = date.getHours() > 12 ? "PM" : "AM";
+                            var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+                            var min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+                            console.log("A bus just passed " + stops[buses[cur_bus].estimate.stop_id] + '\nNext stop: ' +
+                                stops[allBuses[i].arrival_estimates[0].stop_id] + '\nETA: ' +
+                                hours + ":" + min + " " + tod);
+
+                            // tweet to your hearts content
+                            var status = "A bus just passed " + stops[buses[cur_bus].estimate.stop_id] + "\nNext stop: " +
+                                stops[allBuses[i].arrival_estimates[0].stop_id] + "\nETA: " +
                                 // find the actual correct variable, not timeEstimate
-                                buses[index].estimates.arrival_estimates[0].timeEstimate;
-                                console.log(buses);
+                                hours + ":" + min + " " + tod;
                             twitter.statuses('update', {
                                     "status": status,
                                 },
@@ -70,9 +82,35 @@ setInterval (function () {
                                     }
                                 }
                             );
-                        }*/
+
+                            buses[cur_bus].estimate = allBuses[i].arrival_estimates[0];
+
+                        }
+
+                        /*
+                        // the tweet
+                        var status = "A bus just passed " + buses[index].prev_stop + ". \nNext stop: " +
+                            buses[index].estimates.arrival_estimates[0].stop_id + "\nETA: " +
+                            // find the actual correct variable, not timeEstimate
+                            buses[index].estimates.arrival_estimates[0].timeEstimate;
+                            console.log(buses);
+                        twitter.statuses('update', {
+                                "status": status,
+                            },
+                            keys.token,
+                            keys.secret,
+                            function (error, data, response) {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    console.log("success");
+                                }
+                            }
+                        );
+                        */
                     }
                 }
+
                 //for (i in buses) {
                     
                 //}
@@ -81,7 +119,7 @@ setInterval (function () {
             }
         }
     );
-}, 1000);
+}, 5000);
 
 var direction = function (bus) {
     // if next stop is west or north of current bus, it's going outbound (westward)
